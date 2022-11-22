4d911d09cfd3a2726bcffbb573a470caa4b0bc18
soeun <soeuny035@naver.com>
2022-11-22 23:08:46 +0900

    festival_search Page 인자 전달

--- app/src/main/java/com/OOP/FeslenderApp/festival_list.kt
+++ app/src/main/java/com/OOP/FeslenderApp/festival_list.kt
@@ -11,6 +11,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.OOP.FeslenderApp.databinding.FragmentFestivalListBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -19,12 +21,24 @@ import com.google.firebase.ktx.Firebase
 class festival_list : Fragment() {
     lateinit var fesdata: ArrayList<FesData>
     lateinit var adapter: FesAdapter
+    lateinit var myRef: DatabaseReference
+
+    private var clickResult: String = "festival"
 
     val database =
         Firebase.database("https://feslender-kotlin-70d17-default-rtdb.asia-southeast1.firebasedatabase.app/")
-    val myRef = database.getReference("festival")
+
 
     var binding: FragmentFestivalListBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            clickResult = it.getString("click") ?: "festival"
+            Log.e("click:", clickResult)
+            myRef = database.getReference(clickResult)
+        }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,29 +50,41 @@ class festival_list : Fragment() {
         binding?.recFestival?.layoutManager = LinearLayoutManager(context)
         binding?.recFestival?.adapter = adapter
 
+
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                for (ds in snapshot.children) {
-                    val myfesData = FesData().apply{
-                        name = ds.child("name").value as String
-                        date = ds.child("date").value as String
-                        end_date = ds.child("end_date").value as String
-                        location = ds.child("location").value as String
-                        poster = ds.child("pos").value as String
+                if (clickResult == "match") {
+                    for (ds in snapshot.children){
+                        var pos = ds.child("pos1").value as String
+                        pos = pos.replace("gs://feslender-kotlin-70d17.appspot.com/","")
+
+                        val myfesData = FesData().apply{
+                            name = "${ds.child("team1").value as String} vs ${ds.child("team2").value as String}"
+                            date = ds.child("date").value as String
+                            end_date = date
+                            location = ds.child("location").value as String
+                            poster = pos
+                        }
+                        fesdata.add(myfesData)
                     }
-                    fesdata.add(myfesData)
-                    /*
-                    val name: String = ds.child("name").value as String
-                    val date: String = ds.child("date").value as String
-                    val endDate: String = ds.child("end_date").value as String
-                    val location: String = ds.child("location").value as String
-                    val poster: String = ds.child("pos").value as String
-
-                    val myfesData = FesData(name, date, location, endDate, poster)
-                    fesdata.add(myfesData)
-
-                     */
                 }
+                else{
+                    for (ds in snapshot.children) {
+                        val myfesData = FesData().apply {
+                            name = ds.child("name").value as String
+                            date = ds.child("date").value as String
+                            if (ds.child("end_data").value != null) {
+                                end_date = ds.child("end_date").value as String
+                            } else {
+                                end_date = date
+                            }
+                            location = ds.child("location").value as String
+                            poster = ds.child("pos").value as String
+                        }
+                        fesdata.add(myfesData)
+                    }
+                }
+
                 adapter.notifyDataSetChanged()
             }
 
--- app/src/main/java/com/OOP/FeslenderApp/festival_search.kt
+++ app/src/main/java/com/OOP/FeslenderApp/festival_search.kt
@@ -5,55 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.OOP.FeslenderApp.databinding.FragmentFestivalListBinding
+import com.OOP.FeslenderApp.databinding.FragmentFestivalSearchBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [festival_search.newInstance] factory method to
- * create an instance of this fragment.
- */
 class festival_search : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    var binding: FragmentFestivalSearchBinding? = null
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_festival_search, container, false)
+        binding = FragmentFestivalSearchBinding.inflate(inflater)
+
+        return binding?.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.btnFestival?.setOnClickListener{
+            val result = "festival"
+            val bundle = bundleOf("click" to result)
+            findNavController().navigate(R.id.action_festival_search_to_festival_list,bundle)
+        }
+        binding?.btnConcert?.setOnClickListener{
+            val result = "concert"
+            val bundle = bundleOf("click" to result)
+            findNavController().navigate(R.id.action_festival_search_to_festival_list,bundle)
+        }
+        binding?.btnMatch?.setOnClickListener{
+            val result = "match"
+            val bundle = bundleOf("click" to result)
+            findNavController().navigate(R.id.action_festival_search_to_festival_list,bundle)
+        }
+
+        /*
+        binding?.btnResult?.setOnClickListener {
+            val result = examineMBTI()
+            val bundle = bundleOf("MBTI" to result)
+            findNavController().navigate(R.id.action_examineFragment_to_resultFragment,bundle)
+        }
+
+         */
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment festival_search.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            festival_search().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
