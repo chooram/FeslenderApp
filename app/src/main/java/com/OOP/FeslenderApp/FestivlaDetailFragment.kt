c5b5422ab4522dce21180a055039a683d190cc91
cozy01 <yuji10085@gamil.com>
2022-12-06 14:13:54 +0900

    유지 디테일 xml 수정 중2

--- app/src/main/java/com/OOP/FeslenderApp/FestivlaDetailFragment.kt
+++ app/src/main/java/com/OOP/FeslenderApp/FestivlaDetailFragment.kt
@@ -1,30 +1,68 @@
 package com.OOP.FeslenderApp
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.OOP.FeslenderApp.databinding.FragmentFestivalDetailBinding
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.collections.ArrayList
 
 class FestivlaDetailFragment : Fragment() {
 
-    val database =
-        Firebase.database("https://feslender-kotlin-70d17-default-rtdb.asia-southeast1.firebasedatabase.app/")
-
+    lateinit var myRef: DatabaseReference
+    lateinit var database: FirebaseDatabase
 
     var binding: FragmentFestivalDetailBinding? = null
 
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        database =
+            Firebase.database("https://feslender-kotlin-70d17-default-rtdb.asia-southeast1.firebasedatabase.app/")
+
+        arguments?.let {
+            clickResult = it.getString("click") ?: ""
+            searchResult = it.getString("search") ?: ""
+            myD = database.getReference()
+        }
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.fesdata.observe(viewLifecycleOwner){
+            binding?.recFestival?.adapter?.notifyDataSetChanged()
+        }
+        binding?.recFestival?.layoutManager = LinearLayoutManager(context)
+        binding?.recFestival?.adapter = FesAdapter(viewModel.fesdata)
+
+        myD.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                for (data in snapshot.children){
+
+                    val  chkMatch = data.key == "match"
+                    Log.e("check Match",chkMatch.toString())
+
+                    val  chkConcert = data.key == "concert"
+                    Log.e("check Concert",chkConcert.toString())
+
+                    val  chkFestival = data.key == "festival"
+                    Log.e("check Festival",chkFestival.toString())
+
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        }
+        )
+    }
+
 }
+
